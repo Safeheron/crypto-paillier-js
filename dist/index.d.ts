b867358e45734cce20ea3c412455700dbe4d2a0b
yanjie <yanjie@renrenbit.com>
2022-08-15 20:46:52 +0800

    configuration ts for mocha, and add github workflow, and update license in package.json and update @safeheron/crypto-rand dependencies.

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { PailPrivKey, PailPubKey, createPailKeyPair, fastPailHandler_AddPlain, fastPailHandler_Decrypt, fastPailHandler_EncryptWithR, fastPailHandler_Mul } from "./lib/paillier";
-export { PailPrivKey, PailPubKey, createPailKeyPair, fastPailHandler_AddPlain, fastPailHandler_Decrypt, fastPailHandler_EncryptWithR, fastPailHandler_Mul };
